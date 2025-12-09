30bc43f6a6567b2d7756ea289ca050b61875388a
BrunoGdai <19419320+BrunoGdai@users.noreply.github.com>
2025-12-08 21:55:18 -0300

    acrescimos nas urls

--- backend/projeto_vidaplus/urls.py
+++ backend/projeto_vidaplus/urls.py
@@ -1,12 +1,15 @@
-from django.contrib import admin
 from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/auth/', include('apps.usuarios.auth_urls')),
     path('api/unidades/', include('apps.unidades.urls')),
     path('api/profissionais/', include('apps.profissionais.urls')),
     path('api/pacientes/', include('apps.pacientes.urls')),
     path('api/leitos/', include('apps.leitos.urls')),
+    path('api/suprimentos/', include('apps.suprimentos.urls')),
+    path('api/movimentacoes/', include('apps.movimentacoes.urls')),
+    path('api/consultas/', include('apps.consultas.urls')),
+    path('api/exames/', include('apps.exames.urls')),
+    path('api/internacoes/', include('apps.internacoes.urls')),
 ]
